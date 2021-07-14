c9a9e70be5af4e3eabfc2231dd28829aa63834dc
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-07-14 11:26:50 +0200

    Interacting with Databases - Implemented findById and findByName - Implemented deleteById, insert and update

--- src/main/java/nl/paulzijlmans/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/nl/paulzijlmans/database/databasedemo/DatabaseDemoApplication.java
@@ -1,5 +1,6 @@
 package nl.paulzijlmans.database.databasedemo;
 
+import nl.paulzijlmans.database.databasedemo.entity.Person;
 import nl.paulzijlmans.database.databasedemo.jdbc.PersonJdbcDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,20 +9,29 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class DatabaseDemoApplication implements CommandLineRunner {
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
-    PersonJdbcDao dao;
+  @Autowired PersonJdbcDao dao;
 
-    public static void main(String[] args) {
-        SpringApplication.run(DatabaseDemoApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DatabaseDemoApplication.class, args);
+  }
 
-    @Override
-    public void run(String... args) {
-        logger.info("All users -> {}", dao.findAll());
-    }
+  @Override
+  public void run(String... args) {
+    logger.info("All users -> {}", dao.findAll());
+    logger.info("User id 10001 -> {}", dao.findById(10001));
+    logger.info("User(s) name Pieter -> {}", dao.findByName("Pieter"));
+    logger.info("Deleting 10002 -> No of Rows Deleted - {}", dao.deleteById(10002));
+    logger.info(
+        "Inserting 10004 -> {}", dao.insert(new Person(10004, "Tara", "Berlin", new Date())));
+    logger.info(
+        "Updating 10003 -> {}", dao.update(new Person(10003, "Pieter", "Utrecht", new Date())));
+    logger.info("All users -> {}", dao.findAll());
+  }
 }
