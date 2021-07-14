81b4b17a51ca6b43174fdef43f1cf9a81e8ec501
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-07-14 10:50:19 +0200

    Interacting with Databases - Implemented findAll using Spring JDBC

--- src/main/java/nl/paulzijlmans/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/nl/paulzijlmans/database/databasedemo/DatabaseDemoApplication.java
@@ -1,13 +1,27 @@
 package nl.paulzijlmans.database.databasedemo;
 
+import nl.paulzijlmans.database.databasedemo.jdbc.PersonJdbcDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DatabaseDemoApplication {
+public class DatabaseDemoApplication implements CommandLineRunner {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    PersonJdbcDao dao;
 
     public static void main(String[] args) {
         SpringApplication.run(DatabaseDemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        logger.info("All users -> {}", dao.findAll());
+    }
 }
